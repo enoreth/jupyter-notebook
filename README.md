e55c84134da36cc2fd5f928f5eee8802da149c77
dk dk dk <devrim.kural@wienerzeitung.at>
2021-01-08 09:53:37 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+![deaths](deaths.jpg)
+
+![previous-deaths](previous-deaths.jpg)
+
+# covid model
+
+For the US data, cases reported today are an accurate predictor of deaths 21-28 days in the future.
+
+## Rationale
+
+The assumption is that the number of deaths today is a function of the number of cases discovered at some point in the past. Using [The COVID Tracking Project's](https://covidtracking.com/data) data and linear regression, find the best correlation between past cases and deaths at a future date.
+
+## Cases
+
+The prediction curves below are a linear function of cases, but offset by a number of days.
+
+![cases](cases.png)
+
+## Daily
+
+<img src="dailycomparison.png" width="66%">
+<img src="daily.png" width="32%">
+
+## Smoothed
+
+<img src="smoothedcomparison.png" width="66%">
+<img src="smoothed.png" width="32%">
---
+++ model.py
@@ -0,0 +1,49 @@
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+from collections import namedtuple
+
+
+Model = namedtuple('Model', 'linearRegression r2 offset data')
+
+
+def BestFitModel(new_cases, new_deaths, max_offset) -> Model:
+    best = Model(None, 0.0, 0, None)
+
+    # find an offset, in days, where cases best correlate to future deaths
+    for offset in range(1, max_offset):
+        cases = new_cases[-0:-offset]
+        deaths = new_deaths[offset:]
+
+        model = LinearRegression().fit(cases, deaths)
+        predictions = model.predict(cases)
+        r2 = metrics.r2_score(deaths, predictions)
+        if (r2 > best.r2):
+            best = Model(model, r2, offset, pd.DataFrame({'predicted': predictions, 'actual': deaths}))
+
+    return best
+
+
+def Predict(model: Model, dates, cases, deaths) -> pd.DataFrame:
+    # create a new date series for the range over which we will predict
+    # (it is wider than the source date range by [offset]. that is how far in the future we can predict)
+    minDate = np.amin(dates)
+    maxDate = np.amax(dates) + np.timedelta64(model.offset + 1, 'D')
+
+    projected_dates = [date for date in np.arange(minDate, maxDate, dt.timedelta(days=1))]
+
+    # padding so actuals and predictions can be graphed together within dates
+    padding = pd.Series(np.full(model.offset, np.nan))
+
+    actual_deaths = deaths.append(padding)
+    projected_deaths = padding.append(pd.Series(model.linearRegression.predict(cases)))
+
+    frame = pd.DataFrame({"dates": projected_dates, 
+        "actual": actual_deaths.values, 
+        "projected": projected_deaths.values}, index=projected_dates)
+
+    # unpivot the data set for easy graphing
+    return frame.melt(id_vars=['dates'], var_name='series', value_name='deaths')        
